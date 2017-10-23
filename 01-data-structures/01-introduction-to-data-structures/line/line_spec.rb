c514fb464aaa3718553c9435c82d9250bad7c05c
Christine Le <christinele.cs@gmail.com>
2017-10-23 11:50:02 -0500

    data structures

--- 01-data-structures/01-introduction-to-data-structures/line/line_spec.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line_spec.rb
@@ -6,57 +6,59 @@ RSpec.describe Line, type: Class do
   let(:line) { Line.new }
 
   before do
+    p "************************"
     line.join("Hillary")
     line.join("Bernie")
     line.join("Donald")
     line.join("Ted")
     line.join("Jeb")
     line.join("Ben")
+    p line
   end
 
-  describe "#initialize" do
-    it "instantiates the members variable" do
-      expect(line.members).to_not be_nil
-    end
-  end
+  # describe "#initialize" do
+  #   it "instantiates the members variable" do
+  #     expect(line.members).to_not be_nil
+  #   end
+  # end
 
-  describe "#front" do
-    it "returns the first person in the line" do
-      expect(line.front).to eq "Hillary"
-    end
-  end
+  # describe "#front" do
+  #   it "returns the first person in the line" do
+  #     expect(line.front).to eq "Hillary"
+  #   end
+  # end
 
-  describe "#middle" do
-    it "returns the person in the middle of the line" do
-      expect(line.middle).to eq "Ted"
-    end
-  end
+  # describe "#middle" do
+  #   it "returns the person in the middle of the line" do
+  #     expect(line.middle).to eq "Ted"
+  #   end
+  # end
 
-  describe "#back" do
-    it "returns the person in the back of the line" do
-      expect(line.back).to eq "Ben"
-    end
-  end
+  # describe "#back" do
+  #   it "returns the person in the back of the line" do
+  #     expect(line.back).to eq "Ben"
+  #   end
+  # end
 
-  describe "#search" do
-    it "finds the person we're searching for" do
-      person = line.search("Ted")
-      expect(person).to eq "Ted"
-    end
-  end
+  # describe "#search" do
+  #   it "finds the person we're searching for" do
+  #     person = line.search("Ted")
+  #     expect(person).to eq "Ted"
+  #   end
+  # end
 
-  describe "#join" do
-    it "adds a person to the back of the line" do
-      line.join("Homer")
-      expect(line.back).to eq "Homer"
-    end
-  end
+  # describe "#join" do
+  #   it "adds a person to the back of the line" do
+  #     line.join("Homer")
+  #     expect(line.back).to eq "Homer"
+  #   end
+  # end
 
-  describe "#leave" do
-    it "removes a person from the line" do
-      line.leave("Donald")
-      expect(line.search("Donald")).to be_nil
-    end
-  end
+  # describe "#leave" do
+  #   it "removes a person from the line" do
+  #     line.leave("Donald")
+  #     expect(line.search("Donald")).to be_nil
+  #   end
+  # end
 
 end
